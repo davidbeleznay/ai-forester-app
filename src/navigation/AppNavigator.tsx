8eda8607a33e2a2a905db1c7179d40f3027599e2
davidbeleznay <dbeleznay09@gmail.com>
2025-04-24 22:07:27 -0700

    fix: add GestureHandlerRootView wrapper to AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet, View } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import InstructionsScreen from '../screens/InstructionsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
@@ -51,90 +53,98 @@ const TabNavigator = () => {
 // Main app navigator with all screens
 const AppNavigator = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#047857',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen 
-        name="Main" 
-        component={TabNavigator} 
-        options={{ headerShown: false }} 
-      />
-      
-      {/* Tool screens */}
-      <Stack.Screen 
-        name="CulvertSizing" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Culvert Sizing',
-          headerBackTitle: 'Back'
-        }} 
-      />
-      <Stack.Screen 
-        name="TreeHealth" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Tree Health Analysis',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="PebbleCount" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Wolman Pebble Count',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="RestorationMonitoring" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Restoration Monitoring',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="RoadInspection" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Road Inspection',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="GullyAssessment" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Gully Assessment',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="FirePredictor" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Fire Predictor',
-          headerBackTitle: 'Back' 
-        }} 
-      />
-      <Stack.Screen 
-        name="LandslidePredictor" 
-        component={PlaceholderScreen} 
-        options={{ 
-          title: 'Landslide Predictor', 
-          headerBackTitle: 'Back' 
-        }} 
-      />
-    </Stack.Navigator>
+    <GestureHandlerRootView style={styles.container}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#047857',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="Main" 
+          component={TabNavigator} 
+          options={{ headerShown: false }} 
+        />
+        
+        {/* Tool screens */}
+        <Stack.Screen 
+          name="CulvertSizing" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Culvert Sizing',
+            headerBackTitle: 'Back'
+          }} 
+        />
+        <Stack.Screen 
+          name="TreeHealth" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Tree Health Analysis',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="PebbleCount" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Wolman Pebble Count',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="RestorationMonitoring" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Restoration Monitoring',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="RoadInspection" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Road Inspection',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="GullyAssessment" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Gully Assessment',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="FirePredictor" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Fire Predictor',
+            headerBackTitle: 'Back' 
+          }} 
+        />
+        <Stack.Screen 
+          name="LandslidePredictor" 
+          component={PlaceholderScreen} 
+          options={{ 
+            title: 'Landslide Predictor', 
+            headerBackTitle: 'Back' 
+          }} 
+        />
+      </Stack.Navigator>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
 
 export default AppNavigator;
