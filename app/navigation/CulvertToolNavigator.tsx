b41e9691cc8ec6483edbb486ef159303e1b82d43
davidbeleznay <dbeleznay09@gmail.com>
2025-04-28 09:06:25 -0700

    Update CulvertToolNavigator with consistent route names

--- app/navigation/CulvertToolNavigator.tsx
+++ app/navigation/CulvertToolNavigator.tsx
@@ -4,14 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Import screens
 import InputScreen from '../screens/CulvertTool/InputScreen';
 import ResultScreen from '../screens/CulvertTool/ResultScreen';
+import HistoryScreen from '../screens/CulvertTool/HistoryScreen';
 import { CulvertFieldCard } from '../utils/storage';
 
 // Define the param list for this navigator
 export type CulvertToolParamList = {
-  Input: undefined;
+  CulvertInput: undefined;
   CulvertResult: {
     fieldCard: CulvertFieldCard;
   };
+  CulvertHistory: undefined;
 };
 
 const Stack = createStackNavigator<CulvertToolParamList>();
@@ -19,13 +21,14 @@ const Stack = createStackNavigator<CulvertToolParamList>();
 const CulvertToolNavigator: React.FC = () => {
   return (
     <Stack.Navigator 
-      initialRouteName="Input"
+      initialRouteName="CulvertInput"
       screenOptions={{
         headerShown: false
       }}
     >
-      <Stack.Screen name="Input" component={InputScreen} />
+      <Stack.Screen name="CulvertInput" component={InputScreen} />
       <Stack.Screen name="CulvertResult" component={ResultScreen} />
+      <Stack.Screen name="CulvertHistory" component={HistoryScreen} />
     </Stack.Navigator>
   );
 };
